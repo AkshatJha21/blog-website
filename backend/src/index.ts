ffb84fb20d33667f037e942ddae9449b041d00fa
AkshatJha21 <akshatj2134@gmail.com>
2024-06-09 22:32:29 +0530

    index cleanup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,4 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign, verify } from 'hono/jwt';
-import { genSalt, hash, compare } from "bcrypt-ts";
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
 
