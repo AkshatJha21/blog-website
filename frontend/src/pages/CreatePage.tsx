4f2541b4ca6369838eb7b3a9c7cb07301d1d563b
AkshatJha21 <akshatj2134@gmail.com>
2024-06-13 18:21:10 +0530

    create page designed

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,6 +1,14 @@
+import { useEffect, useRef } from "react"
 import Navbar from "../components/Navbar"
 
 const CreatePage = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, []);
+
+    
   return (
     <div>
         <Navbar 
@@ -11,9 +19,9 @@ const CreatePage = () => {
             primaryClick={() => {}}
             secondaryClick={() => {}}
         />
-        <div className="flex flex-col">
-            <input type="text" placeholder="title"/>
-            <input type="text" placeholder="content"/>
+        <div className="flex flex-col w-[90%] mx-auto justify-start my-4 p-4">
+            <input ref={inputRef} className="text-2xl font-bold mb-4 p-1 focus:outline-none" type="text" placeholder="Enter a title"/>
+            <textarea rows={20} className="focus:outline-none resize-none p-1 text-lg mt-4 font-serif" placeholder="Write your blog here"/>
         </div>
     </div>
   )
