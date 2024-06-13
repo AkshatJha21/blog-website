849ab5b9f8722b722093adbfec3a6db2d3f453dc
AkshatJha21 <akshatj2134@gmail.com>
2024-06-13 15:16:28 +0530

    fetching works but rendering errors

--- frontend/src/pages/ReadPage.tsx
+++ frontend/src/pages/ReadPage.tsx
@@ -1,7 +1,27 @@
+import { useParams } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 const ReadPage = () => {
-    
+    const { id } = useParams();
+    const [blog, setBlog] = useState({});
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        
+        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            setBlog(response.data.post);
+        }).catch(error => {
+            console.error('Error fetching blog: ' + error);
+        });
+    }, []);
+
   return (
     <div>
         <Navbar 
@@ -12,6 +32,11 @@ const ReadPage = () => {
             primaryClick={() => {}}
             secondaryClick={() => {}}
         />
+        <div>
+            <h2>{blog.title}</h2>
+            <p>{blog.authorId}</p>
+            <p>{blog.content}</p>
+        </div>
     </div>
   )
 }
