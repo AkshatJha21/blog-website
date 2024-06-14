acf2d254a1ca8b5b735a6dc295ec2c2bbf173142
AkshatJha21 <akshatj2134@gmail.com>
2024-06-14 12:08:02 +0530

    dropdown designed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -39,15 +39,15 @@ const Navbar = ({
                 <div className="relative">
                     <div onClick={toggleMenu} className="bg-neutral-100 font-medium rounded-full h-10 w-10 p-1 flex items-center justify-center cursor-pointer hover:bg-neutral-200 hover:border-neutral-500 hover:text-black text-neutral-500 border-2 border-neutral-400 transition-all">U</div> 
                     {isMenuVisible && (
-                        <div className="absolute bg-white right-0 mt-2 w-48 shadow-lg p-2 z-20 rounded-sm">
-                            <div>
-                                User Name
-                            </div>
-                            <ul className="py-1">
-                                <li>
+                        <div className="absolute bg-white right-0 mt-2 w-48 shadow-md border p-2 z-20 rounded-sm">
+                            <ul>
+                                <li className="p-2 hover:bg-neutral-100 rounded-sm transition-all cursor-pointer">
                                     Profile
                                 </li>
-                                <li>
+                                <li className="p-2 hover:bg-neutral-100 rounded-sm transition-all cursor-pointer">
+                                    Posts
+                                </li>
+                                <li className="p-2 hover:bg-neutral-100 rounded-sm transition-all cursor-pointer">
                                     Logout
                                 </li>
                             </ul>
