e500feb118e8ecfee32c3b3210d70540bdf42ca6
AkshatJha21 <akshatj2134@gmail.com>
2024-06-22 22:24:13 +0530

    home page complete

--- frontend/src/components/BlogPreview.tsx
+++ frontend/src/components/BlogPreview.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface BlogPreviewProps {
@@ -16,12 +17,7 @@ export const BlogPreview = ({
   blogId
 }: BlogPreviewProps) => {
   return (
-    <div className="w-[70%] mx-auto hover:bg-neutral-100 p-4 border-b hover:cursor-pointer">
-        <button>
-          <svg className="w-6 h-6 text-neutral-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeWidth="3" d="M12 6h.01M12 12h.01M12 18h.01"/>
-          </svg>
-        </button>
+    <div className="w-[70%] mx-auto p-4 border-b hover:bg-neutral-100 transition">
       <Link to={`/read/${blogId}`}>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-x-2 mb-1">
