1788738229ae661d572f2c9f7073d1c798304190
AkshatJha21 <akshatj2134@gmail.com>
2024-06-22 17:00:36 +0530

    follower/following count fixed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = ({
             <h2 className="flex font-black text-xl sm:text-2xl p-4 cursor-pointer" onClick={handleHome}>
                 TheBlog
                 <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M6 6a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3H5a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2H6Zm9 0a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3h-1a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2h-3Z" clip-rule="evenodd"/>
+                    <path fillRule="evenodd" d="M6 6a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3H5a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2H6Zm9 0a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3h-1a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2h-3Z" clipRule="evenodd"/>
                 </svg>
             </h2>
             <div className="p-4 flex items-center space-x-4">
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar"
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 import { useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
+import NoResult from "../components/NoResult";
 
 interface User {
     id: string;
@@ -13,6 +15,9 @@ interface User {
 const ProfilePage = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [focus, setFocus] = useState("Followers");
+    const [loading, setLoading] = useState(true);
+    const [followers, setFollowers] = useState(0);
+    const [following, setFollowing] = useState(0);
     const navigate = useNavigate();
 
     const handleFocusChange = (button: string) => {
@@ -21,6 +26,27 @@ const ProfilePage = () => {
 
     useEffect(() => {
         const token = localStorage.getItem('token');
+        setLoading(true);
+
+        axios.get(`${BACKEND_URL}/api/v1/user/me/followers`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            setFollowers(response.data.users.length);
+        }).catch(error => {
+            console.error('Error fetching follower count: ' + error);
+        });
+
+        axios.get(`${BACKEND_URL}/api/v1/user/me/following`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            setFollowing(response.data.users.length);
+        }).catch(error => {
+            console.error('Error fetching following count: ' + error);
+        });
 
         let route = '/followers';
 
@@ -34,8 +60,10 @@ const ProfilePage = () => {
             }
         }).then(response => {
             setUsers(response.data.users);
+            setLoading(false);
         }).catch(err => {
             console.error("Error fetching users: " + err);
+            setLoading(false);
         });
     }, [focus]);
   return (
@@ -51,26 +79,34 @@ const ProfilePage = () => {
               onClick={() => handleFocusChange("Followers")} 
               className={`${focus === "Followers" ? "text-black border-b-black" : "text-neutral-400"} bg-transparent font-medium px-4 py-2 border-b border-neutral-300`}
             >
-                <p className="text-xl font-semibold">{users.length}</p>
+                <p className="text-xl font-semibold">{followers}</p>
                 Followers
             </button>
             <button
               onClick={() => handleFocusChange("Following")} 
               className={`${focus === "Following" ? "text-black border-b-black" : "text-neutral-400"} bg-transparent font-medium px-4 py-2 border-b border-neutral-300`}
             >
-                <p className="text-xl font-semibold">{users.length}</p>
+                <p className="text-xl font-semibold">{following}</p>
                 Following
             </button>
         </div>
-        {users.map((user) => (
-            <div key={user.id} className="mx-auto flex flex-col py-2 px-4 border-b">
-                <div className="flex items-center gap-x-2">
-                    <div className="flex bg-neutral-200 h-8 w-8 text-sm items-center rounded-full p-2 justify-center">{user.name[0]}</div>
-                    <h2 className="font-medium">{user.name}</h2>
-                </div>
-                <p className="text-neutral-500 font-light">{user.email}</p>
-            </div>
-        ))}
+        {loading ? (
+            <Loader />
+        ) : users.length === 0 ? (
+            <NoResult />
+        ) : (
+            users.map((user) => {
+                return (
+                    <div key={user.id} className="mx-auto flex flex-col py-2 px-4 border-b">
+                        <div className="flex items-center gap-x-2">
+                            <div className="flex bg-neutral-200 h-8 w-8 text-sm items-center rounded-full p-2 justify-center">{user.name[0]}</div>
+                            <h2 className="font-medium">{user.name}</h2>
+                        </div>
+                        <p className="text-neutral-500 font-light">{user.email}</p>
+                    </div>
+                )
+            })
+        )}
         </div>
     </div>
   )
