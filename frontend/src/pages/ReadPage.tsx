f16b48d985cecd897b170795a36cc12b8a74e346
AkshatJha21 <akshatj2134@gmail.com>
2024-06-22 22:38:03 +0530

    skeleton added

--- frontend/src/pages/ReadPage.tsx
+++ frontend/src/pages/ReadPage.tsx
@@ -13,6 +13,7 @@ import { BACKEND_URL } from "../config";
 const ReadPage = () => {
     const { id } = useParams();
     const [blog, setBlog] = useState<Blog>({title: "", content: "", author: ""});
+    const [isLoading, setIsLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -24,8 +25,10 @@ const ReadPage = () => {
             }
         }).then(response => {
             setBlog(response.data.blog);
+            setIsLoading(false);
         }).catch(error => {
             console.error('Error fetching blog: ' + error);
+            setIsLoading(false);
             navigate('/');
         });
     }, []);
@@ -37,19 +40,40 @@ const ReadPage = () => {
             showPlus={true} 
             primaryClick={() => navigate('/create')}
         />
-        <div className="w-[90%] flex flex-col mx-auto my-4">
-            <h2 className="text-3xl font-bold mb-2">{blog.title}</h2>
-            <div className="flex justify-end items-center gap-x-2 my-2">
-                <p>Written by</p>
-                <div className="p-2 bg-neutral-200 text-sm rounded-full w-8 h-8 flex items-center justify-center">
-                    {blog.author[0]}
+        {isLoading ? (
+            <ReadPageSkeleton />
+        ): (
+            <div className="w-[90%] flex flex-col mx-auto my-4">
+                <h2 className="text-3xl font-bold mb-2">{blog.title}</h2>
+                <div className="flex justify-end items-center gap-x-2 mt-2 mb-4">
+                    <p className="text-sm font-medium text-neutral-600 mr-2">Written by</p>
+                    <div className="p-2 bg-neutral-200 text-sm rounded-full w-8 h-8 flex items-center justify-center">
+                        {blog.author[0]}
+                    </div>
+                    <p className="font-medium">{blog.author}</p>
                 </div>
-                <p className="font-medium">{blog.author}</p>
+                <p className="font-serif text-lg">{blog.content}</p>
             </div>
-            <p className="font-serif text-lg">{blog.content}</p>
-        </div>
+        )}
     </div>
   )
+}
+
+const ReadPageSkeleton = () => {
+    return (
+        <div className="w-[90%] flex flex-col mx-auto my-4 animate-pulse">
+            <div className="h-8 bg-neutral-400 rounded mb-2"></div>
+            <div className="flex justify-end items-center gap-x-2 mt-2 mb-4">
+                <div className="h-6 w-20 bg-neutral-400 rounded"></div>
+                <div className="p-2 bg-neutral-400 text-sm rounded-full w-8 h-8 flex items-center justify-center"></div>
+                <div className="h-6 w-32 bg-neutral-400 rounded"></div>
+            </div>
+            <div className="h-4 bg-neutral-400 rounded mb-2"></div>
+            <div className="h-4 bg-neutral-400 rounded mb-2"></div>
+            <div className="h-4 bg-neutral-400 rounded mb-2"></div>
+            <div className="h-4 bg-neutral-400 rounded mb-2"></div>
+        </div>
+    )
 }
 
 export default ReadPage
